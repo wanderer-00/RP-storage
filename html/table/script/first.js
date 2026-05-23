9dff37ec50237594190d0fd525ea8e23dc968cc6
wanderer <56039464+wanderer-00@users.noreply.github.com>
2026-05-23 14:48:52 +0300

    -

--- html/table/script/first.js
+++ html/table/script/first.js
@@ -5,13 +5,12 @@ let firstTbody = document.createElement('tbody');
 for(i=0;i<firstData.length;i++){
     firstTbody.innerHTML += `
         <tr>
-            <td>${firstData[i][6]}</td>
-            <td align="center">${firstData[i][0]}</td>
-            <td align="center">${firstData[i][1]}</td>
-            <td align="right">${firstData[i][2]}</td>
-            <td align="right">${firstData[i][3]}</td>
-            <td align="right">${firstData[i][4]}</td>
-            <td>${dateConverter(firstData[i][5])}</td>
+            <td>${firstData[i][5]}</td>
+            <td>${firstData[i][0]}</td>
+            <td>${firstData[i][1]}</td>
+            <td>${firstData[i][2]}</td>
+            <td>${firstData[i][3]}</td>
+            <td>${firstData[i][4]}</td>
         </tr>
 `};
 
@@ -23,13 +22,11 @@ let Table_1_head = document.getElementById('firstTable_head');
 
 Table_1_head.innerHTML += `
         <tr>
-            <th align="left" >Total</th>
+            <th align="left">Total</th>
             <th align="right">${sumMoney(0)}</th>
             <th align="right">${sumMoney(1)}</th>
             <th align="right">${sumMoney(2)}</th>
             <th align="right">${1}</th>
-            <th align="right">${1}</th>
-            <th align="left" ></th>
         </tr>
 `;
 
